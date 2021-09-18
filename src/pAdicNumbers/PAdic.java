36e599d64cadaadc81560e52df827cbc83dcec2a
Maciej Raczuk <mr429173@students.mimuw.edu.pl>
2021-09-18 19:08:24 +0200

    pre alpha 0.1
    -added parsing from string
    -added some comments

--- src/pAdicNumbers/PAdic.java
+++ src/pAdicNumbers/PAdic.java
@@ -12,17 +12,65 @@ public class PAdic {
     private final BigInteger num;
     private final int val;
     private final int p;
-    private static int precision = 1000;
+    private static int precision = 1000; //< Number of relevant digits (in Q_p)
+    //Please note that precision parameter determines computation precision only
+    //and that creating p-adic numbers with higher than set precision is always
+    //possible via constructors. Also it does not influence already created numbers.
     
     private static BigInteger almostZero;
     private static boolean updateNeeded = true;
-
-    public PAdic(String s, int val, int p){
-        num = new BigInteger(s);
-        this.val = val;
+    
+    //Please note that (so far ?) constructors don't check whether given parameters
+    //are correct / perform only partial validation of arguments, thus
+    //incorrect parameters yield undefined behaviour
+    
+    /**
+     * Constructs p-adic number from string with its digits
+     * @param s non-empty string with digits in Q_p of a given numbers
+     * @param p prime
+     */
+    public PAdic(String s, int p){
+        int val = 0;
+        boolean nonZeroOccured = false;
+        boolean dotOccured = false;
+        int stop = 0;
+        for (int i = s.length() - 1; i >= 0; i--){
+            char current = s.charAt(i);
+            if (Character.isLetterOrDigit(current)){
+                if (!nonZeroOccured && current != '0') {
+                    nonZeroOccured = true;
+                    stop = i;
+                    val += s.length() - 1 - i;
+                    if (dotOccured) val -= 1;
+                }
+                if (PAdic.charToByte(current) >= p) 
+                    throw new RuntimeException("Cannot parse string: " + s + " to p-adic integer.");
+            }
+            else if (current == '.' && !dotOccured) {
+                val -= s.length() - 1 - i;
+                dotOccured = true;
+            }
+            else throw new RuntimeException("Cannot parse string: " + s + " to p-adic integer.");
+        }
+        BigInteger num = BigInteger.ZERO;
+        for (int i = 0; i <= stop; i++){
+            if (s.charAt(i) == '.') continue;
+            num = num.multiply(BigInteger.valueOf(p));
+            num = num.add(BigInteger.valueOf(PAdic.charToByte(s.charAt(i))));
+        }
+        this.num = num;
+        if (num.equals(BigInteger.ZERO)) this.val = Integer.MAX_VALUE;
+        else this.val = val;
         this.p = p;
     }
 
+    /**
+     * Constructs p-adic number from n not divisible by p
+     * and it's p-adic valuation val (or n = 0 and whatever)
+     * @param n  BigInteger
+     * @param val p-adic valuation
+     * @param p prime
+     */
     public PAdic(BigInteger n, int val, int p){
         num = n;
         if (num.equals(BigInteger.ZERO)) this.val = Integer.MAX_VALUE;
@@ -46,6 +94,15 @@ public class PAdic {
         if (num.equals(BigInteger.ZERO)) val1 = Integer.MAX_VALUE;
         this.val = val1;
         this.p = p;
+    }
+
+    /**
+     * Constructs p-adic number from integer
+     * @param m value of p-adic number
+     * @param p prime
+     */
+    public PAdic(int m, int p){
+        this(BigInteger.valueOf(m), p);
     }
 
     private static byte charToByte(char c){
@@ -83,7 +140,16 @@ public class PAdic {
         }
         return almostZero;
     }
-    
+
+    /**
+     * Returns a sum of this and given p-adic number.
+     * IMPLEMENTATION:
+     * We can see that it is easy to add 2 non divisible by p elements of Z_p
+     * In general case of Q_p we can just multiply the arguments by proper
+     * power of p and easily calculate p-adic valuation of the result.
+     * @param a p-adic number
+     * @return sum
+     */
     public PAdic add(PAdic a){
         if (p != a.p) throw new FieldError(p, a.p);
         int diff = val - a.val;
@@ -100,13 +166,35 @@ public class PAdic {
         return new PAdic(sumNum, sumVal, p);
     }
     
+    public PAdic add(int a){
+        return this.add(new PAdic(a, p));
+    }
+
+    /**
+     * Returns a product of this and given p-adic number.
+     * IMPLEMENTATION:
+     * Similarly to addition we can just multiply nums and
+     * then calculation of p-adic valuation of the result is straightforward
+     * from its definition.
+     * @param a p-adic number
+     * @return product
+     */
     public PAdic mul(PAdic a){
         BigInteger prodNum = this.num.multiply(a.num);
         int prodVal = this.val + a.val;
         prodNum = prodNum.mod(this.getAlmostZero());
         return new PAdic(prodNum, prodVal, p);
     }
-    
+
+    public PAdic mul(int a){
+        return this.mul(new PAdic(a, p));
+    }
+
+    /**
+     * Regular substraction is sufficient after choosing
+     * sufficiently large power of p (p^N ≈ 0 for large N)
+     * @return negation
+     */
     public PAdic neg(){
         BigInteger negNum = this.num;
         negNum = this.getAlmostZero().subtract(negNum);
@@ -115,6 +203,9 @@ public class PAdic {
     
     public PAdic sub(PAdic a){
         return this.add(a.neg());
+    }
+    public PAdic sub(int a){
+        return this.sub(new PAdic(a, p));
     }
     
     public int[] getDigits(){
@@ -149,7 +240,15 @@ public class PAdic {
         }
         return out;
     }
-    
+
+    /**
+     * Firstly we calculate U_0 - inverse mod p of non-divisible by p
+     * element of Z_p and then we raise it to inverse mod p^(2^n)
+     * using recurrence relation U_{n_1} = U_n(2 - a*U_n), where a
+     * is number to be inverted. After that valuation of the result
+     * can be easily calculated
+     * @return inverse
+     */
     public PAdic inv(){
         int n = logFloor2(precision) + 1;
         BigInteger TWO = BigInteger.valueOf(2);
@@ -166,6 +265,10 @@ public class PAdic {
     public PAdic div(PAdic a){
         if (a.num.equals(BigInteger.ZERO)) throw new RuntimeException("Division by zero");
         return this.mul(a.inv());
+    }
+
+    public PAdic div(int a){
+        return this.div(new PAdic(a, p));
     }
     
     public void print(int digits){
